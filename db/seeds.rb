7568d1b9cc71d5e68dc19cf1607d86dae8913e98
ryanadams <rcoleman.adams@gmail.com>
2022-05-25 13:50:30 -0500

    commit tree 2a0efb0d0146fcd77b85d84d4f91abe2a5c3e309

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,27 @@ Artist.delete_all
 
 hotline_bling = Song.create(name: 'Hotline Bling')
 thriller = Song.create(name: 'Thriller')
+over = Song.create(name: 'Over')
+animal = Song.create(name: 'Animal')
 
 drake = Artist.create(name: 'Drake')
 mj = Artist.create(name: 'Michael Jackson')
+maroon5 = Artist.create(name: 'Maroon 5')
 
 rap = Genre.create(name: 'Rap')
 pop = Genre.create(name: 'Pop')
 
 hotline_bling.artist = drake
 thriller.artist = mj
+over.artist = drake
+animal.artist = maroon5
 
 drake.songs << hotline_bling
+drake.songs << over
 mj.songs << thriller
+maroon5.songs << animal
 
 pop.songs << thriller
+pop.songs << animal
 rap.songs << hotline_bling
+rap.songs << over
